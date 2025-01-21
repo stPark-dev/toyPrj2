0a9a4d7d8b3c26437edc5c192a279010a784f83f
stpark <dmc6618@gractor.com>
2025-01-21 11:53:05 +0900

    feat: kakao map and backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+      {
+        protocol: "https",
+        hostname: "phinf.pstatic.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/dn/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
